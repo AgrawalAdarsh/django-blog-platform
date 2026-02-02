8b99aa18bee9edefddc899131f44d28b823c28e7
ADARSH AGRAWAL <130668380+AgrawalAdarsh@users.noreply.github.com>
2026-02-03 00:03:46 +0530

    Delete blog_project directory

--- blog_project/comments/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-from users.models import User
-from posts.models import Post
-
-class Comment(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    post = models.ForeignKey(Post, on_delete=models.CASCADE)
-    text = models.TextField()
-    created_at = models.DateTimeField(auto_now_add=True)
